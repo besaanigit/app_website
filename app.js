392f3dc3d38019c25ffaafb664c22d2f042e7476
ibraahinaxmedciise@gmail.com <besaani>
2018-10-25 11:29:20 +0300

    changed some style

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 var nodemailer= require('nodemailer');
 var indexRouter = require('./routes/index');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local').strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/app_website');
+var db = mongoose.connection;
 var about = require('./routes/about');
 var contact = require('./routes/contact');
+var news  = require('./routes/news');
+var Education= require('./routes/education');
+var login = require('./routes/login');
+var register = require('./routes/register');
 var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.set('view engine','jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +38,11 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about',about);
 app.use('/Contact',contact);
+app.use('/news',news);
+app.use('/education',Education);
+app.use('/login',login);
+app.use('/register',register);
+//set static folder
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/contact.js
+++ routes/contact.js
@@ -4,36 +4,37 @@ var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Contact', { title: 'contact' });
+  res.render('contact', { title: 'Contact' });
 });
 //post router
-router.post('/send', function(reg,res,next){
+router.post('/send', function(req, res, next){
  var transport = nodemailer.createTransport({
     //service and crodontials.
-    service:'Gmail',
+    service:'gmail',
     auth: {
     	user: 'moh.abdilatiif@gmail.com',
     	pass: 'somepasswd'
     }
 
  });
-   var mail0p= {
+   var mailOptions = {
    	from: 'besaani - <moh.abdilatiif@gmail.com>',
    	to: 'ibraahinaxmedciise@gmail.com',
-   	subjec: 'Contact from besaani.com',
+   	subject: 'Contact from besaani.com',
    	//plain text version.
-   	text: 'You have new message from'+ reg.body.name +'@besaani.com\n' +'email:'+ reg.body.email +'Message:' + reg.body.message,//ve have name,email and message.
-   	html: '<h3>You have a new message !</h3><br/><ul><li>from :'+ reg.body.name + '@besaani.com</li><li>' + '@email :' + reg.body.email + '</li><li><p>' + reg.body.message + '</p></li></ul>'
-   };
+   	text: 'You have new message from'+ req.body.name +'@besaani.com\n' +'email:'+ req.body.email +'Message:' + req.body.message,//ve have name,email and message.
+   	html: '<h3>You have a new message !</h3><br/><ul><li>from :'+ req.body.name + '@besaani.com</li><li>' + 'Email :' + req.body.email + '</li><li><p>' + req.body.message + '</p></li></ul>'
+   }
 
-   transport.sendMail(mail0p, function (error,info) {
+   transport.sendMail(mailOptions, function (error,info) {
    	  if(error){
-   	  	console.log("Email could not be sent !\n" + error);
+   	  	console.log('Email could not be sent !\n' + error);
    	  	res.redirect ('/');
 
    	} else{
-   		console.log("Message sent succesfully !\n"+ info.response );
+   		console.log('Message sent succesfully !\n' + info.response );
    		res.redirect('/');
+   		
    	}
 
    });
